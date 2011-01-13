ea0c28b1ff36419ce9e5642129180969f8679eae
kresomania <kresimir.bagaric@gmail.com>
2011-01-13 09:04:29 +0100

    initial commit

---
+++ src/main/java/local/kresi/image2table/App.java
@@ -0,0 +1,13 @@
+package local.kresi.image2table;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
